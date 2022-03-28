4767e869aa3b2c7c48252a6f014c62bdbf0cd1f6
mersallCeq <Ahmedmersal44@gmail.com>
2022-03-28 17:30:46 +0200

    addlistner

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var proxy = require("html2canvas-proxy")
 var express = require("express")
+var port = process.env.PORT || 4000
 
 var app = express()
 app.use("/", proxy())
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`)
+})
